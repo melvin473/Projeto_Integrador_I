43f8714c93fc2e202688229cfdd02399a450a224
melvin473 <31097332+melvin473@users.noreply.github.com>
2024-06-08 12:15:33 -0300

    Update settings.py

--- petResgate/settings.py
+++ petResgate/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)xxb&ccs(%qa#cab7b^#^71)_t#fo7@yl)#)^nkk)bop$r1qcj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
